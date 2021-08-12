b80a0e28babb6aff4faf9bbc9c68b9ee639ced36
cryptomeda <82532221+cryptomeda@users.noreply.github.com>
2021-08-12 03:22:20 +0200

    Resolution of findings from audit - part1
    TTV-01	Mutability
    TTV-02	Declaration naming conventions
    TTV-03	Order of Layout
    TTV-04	Struct optimization
    TTV-05	Lack of error messages
    TTV-06	Data location can be calldata
    TTV-07	External calls inside loop
    TTV-08	Re-entrancy
    TTV-09	Zero transfer
    NEW-01	New Vgroup enum values

--- TechTokenVesting.sol
+++ TechTokenVesting.sol
@@ -12,37 +12,24 @@ https://gist.github.com/rstormsf/7cfb0c6b7a835c0c67b4a394b4fd9383
 */
 contract TechTokenVesting {
 
-    ERC20 public techToken;
-    address public owner_;
-
-    modifier onlyAdmin {
-        require(msg.sender == owner_, "Not Admin");
-        _;
-    }
-
-    modifier onlyValidAddress(address _recipient) {
-        require(_recipient != address(0) && _recipient != address(this) && _recipient != address(techToken), "not valid _recipient");
-        _;
-    }
-
-    uint24 constant internal SECONDS_PER_DAY = 86400;
-
     event GrantAdded(address indexed recipient, uint256 grantId);
     event GrantTokensClaimed(address indexed recipient, uint256 amountClaimed);
     event GrantRemoved(address recipient, uint256 amountVested, uint256 amountNotVested);
     event ChangedAdmin(address admin);
 
-    enum VGroup{ Ecosystem_community, 
-                Development_Tech, 
-                Development_Mktg, 
-                Founders, 
-                Team, 
-                Advisors, 
-                DEX_Liquidity, 
-                Seed, 
-                Private_TGE, 
-                Private_Linear, 
-                Public}
+    enum VGroup{ Ecosystem_community,
+                Development_Tech,
+                Development_Mktg,
+                Founders,
+                Team,
+                Advisors,
+                DEX_Liquidity,
+                Seed,
+                Private_TGE,
+                Private_Linear,
+                Public_TGE,
+                Public_Linear,
+                Custom}
 
     struct Grant {
         uint256 startTime;
@@ -50,8 +37,8 @@ contract TechTokenVesting {
         uint16 vestingDuration; // In months
         uint16 vestingCliff;    // In months
         uint16 daysClaimed;
-        uint256 totalClaimed;
         address recipient;
+        uint256 totalClaimed;
     }
 
     // Category of Vesting Group    
@@ -65,11 +52,25 @@ contract TechTokenVesting {
     mapping (address => uint[]) private activeGrants;
     mapping (VGroup => VestingGroup) private parameter; // Enum mapped to Struct
 
+    
+    address public owner;
     uint256 public totalVestingCount = 1;
+    ERC20 public immutable techToken;
+    uint24 constant internal SECONDS_PER_DAY = 86400;
+
+    modifier onlyAdmin {
+        require(msg.sender == owner, "Not Admin");
+        _;
+    }
+
+    modifier onlyValidAddress(address _recipient) {
+        require(_recipient != address(0) && _recipient != address(this) && _recipient != address(techToken), "not valid _recipient");
+        _;
+    }
 
     constructor(ERC20 _techToken)  {
-        require(address(_techToken) != address(0));
-        owner_ = msg.sender;
+        require(address(_techToken) != address(0), "invalid token address");
+        owner = msg.sender;
         techToken = _techToken;
     }
 
@@ -90,19 +91,18 @@ contract TechTokenVesting {
     /// @param _name Vesting group name, which is mapped to its specific parameters 
     /// @param _startTime Grant start time in seconds (unix timestamp)
     /// @param _amount Total number of tokens in grant
-    function addTokenGrant(address[] memory _recipient, 
-                            VGroup[] memory _name, 
-                           uint256[] memory _startTime,
-                           uint256[] memory _amount)
+    function addTokenGrant(address[] calldata _recipient, 
+                            VGroup[] calldata _name, 
+                           uint256[] calldata _startTime,
+                           uint256[] calldata _amount)
                             external onlyAdmin {
+        require(_recipient.length <= 20, "Limit of 20 grants in one call exceeded");
         require(_recipient.length == _name.length, "Different array length");
         require(_recipient.length == _startTime.length, "Different array length");
         require(_recipient.length == _amount.length, "Different array length");
-
+        
         for(uint i=0;i<_recipient.length;i++) {
-
-            // Transfer the grant tokens under the control of the vesting contract
-            require(techToken.transferFrom(owner_, address(this), _amount[i]), "transfer failed");
+            require(_amount[i] > 0, "Amount <= 0");
 
             Grant memory grant = Grant({
                 startTime: _startTime[i] == 0 ? currentTime() : _startTime[i],
@@ -116,9 +116,68 @@ contract TechTokenVesting {
 
             tokenGrants[totalVestingCount] = grant;
             activeGrants[_recipient[i]].push(totalVestingCount);
+
+            // Transfer the grant tokens under the control of the vesting contract
+            require(techToken.transferFrom(owner, address(this), _amount[i]), "transfer failed");
+
             emit GrantAdded(_recipient[i], totalVestingCount);
             totalVestingCount++;    //grantId
         }
+    }
+
+    /// @notice Allows a grant recipient to claim their vested tokens. Errors if no tokens have vested
+    /// It is advised recipients check they are entitled to claim via `calculateGrantClaim` before calling this
+    function claimVestedTokens(uint256 _grantId) external {
+        uint16 timeVested;
+        uint256 amountVested;
+        (timeVested, amountVested) = calculateGrantClaim(_grantId);
+        require(amountVested > 0, "amountVested is 0");
+
+        Grant storage tokenGrant = tokenGrants[_grantId];
+        tokenGrant.daysClaimed = uint16(tokenGrant.daysClaimed+(timeVested));
+        tokenGrant.totalClaimed = uint256(tokenGrant.totalClaimed+(amountVested));
+
+        require(techToken.transfer(tokenGrant.recipient, amountVested), "no tokens");
+        emit GrantTokensClaimed(tokenGrant.recipient, amountVested);
+    }
+
+    /// @notice Terminate token grant transferring all vested tokens to the `_grantId`
+    /// and returning all non-vested tokens to the Admin
+    /// Secured to the Admin only
+    /// @param _grantId grantId of the token grant recipient
+    function removeTokenGrant(uint256 _grantId) 
+        external 
+        onlyAdmin
+    {
+        Grant storage tokenGrant = tokenGrants[_grantId];
+        address recipient = tokenGrant.recipient;
+        uint16 timeVested;
+        uint256 amountVested;
+        (timeVested, amountVested) = calculateGrantClaim(_grantId);
+
+        uint256 amountNotVested = (tokenGrant.amount-(tokenGrant.totalClaimed))-(amountVested);
+
+        tokenGrant.startTime = 0;
+        tokenGrant.amount = 0;
+        tokenGrant.vestingDuration = 0;
+        tokenGrant.vestingCliff = 0;
+        tokenGrant.daysClaimed = 0;
+        tokenGrant.totalClaimed = 0;
+        tokenGrant.recipient = address(0);
+
+        if (amountVested > 0) require(techToken.transfer(recipient, amountVested), "transfer of vested tokens failed");
+        if (amountNotVested > 0) require(techToken.transfer(owner, amountNotVested), "transfer of not-vested tokens failed");
+
+        emit GrantRemoved(recipient, amountVested, amountNotVested);
+    }
+
+    function changeAdmin(address _newAdmin) 
+        external 
+        onlyAdmin
+        onlyValidAddress(_newAdmin)
+    {
+        owner = _newAdmin;
+        emit ChangedAdmin(_newAdmin);
     }
 
     function getActiveGrants(address _recipient) public view returns(uint256[] memory){
@@ -156,52 +215,6 @@ contract TechTokenVesting {
         }
     }
 
-    /// @notice Allows a grant recipient to claim their vested tokens. Errors if no tokens have vested
-    /// It is advised recipients check they are entitled to claim via `calculateGrantClaim` before calling this
-    function claimVestedTokens(uint256 _grantId) external {
-        uint16 timeVested;
-        uint256 amountVested;
-        (timeVested, amountVested) = calculateGrantClaim(_grantId);
-        require(amountVested > 0, "amountVested is 0");
-
-        Grant storage tokenGrant = tokenGrants[_grantId];
-        tokenGrant.daysClaimed = uint16(tokenGrant.daysClaimed+(timeVested));
-        tokenGrant.totalClaimed = uint256(tokenGrant.totalClaimed+(amountVested));
-
-        require(techToken.transfer(tokenGrant.recipient, amountVested), "no tokens");
-        emit GrantTokensClaimed(tokenGrant.recipient, amountVested);
-    }
-
-    /// @notice Terminate token grant transferring all vested tokens to the `_grantId`
-    /// and returning all non-vested tokens to the Admin
-    /// Secured to the Admin only
-    /// @param _grantId grantId of the token grant recipient
-    function removeTokenGrant(uint256 _grantId) 
-        external 
-        onlyAdmin
-    {
-        Grant storage tokenGrant = tokenGrants[_grantId];
-        address recipient = tokenGrant.recipient;
-        uint16 timeVested;
-        uint256 amountVested;
-        (timeVested, amountVested) = calculateGrantClaim(_grantId);
-
-        uint256 amountNotVested = (tokenGrant.amount-(tokenGrant.totalClaimed))-(amountVested);
-
-        require(techToken.transfer(recipient, amountVested));
-        require(techToken.transfer(owner_, amountNotVested));
-
-        tokenGrant.startTime = 0;
-        tokenGrant.amount = 0;
-        tokenGrant.vestingDuration = 0;
-        tokenGrant.vestingCliff = 0;
-        tokenGrant.daysClaimed = 0;
-        tokenGrant.totalClaimed = 0;
-        tokenGrant.recipient = address(0);
-
-        emit GrantRemoved(recipient, amountVested, amountNotVested);
-    }
-
     function currentTime() public view returns(uint256) {
         return block.timestamp;
     }
@@ -209,15 +222,6 @@ contract TechTokenVesting {
     function tokensVestedPerDay(uint256 _grantId) public view returns(uint256) {
         Grant memory tokenGrant = tokenGrants[_grantId];
         return tokenGrant.amount/(uint256(tokenGrant.vestingDuration*(30)));
-    }
-
-    function changeAdmin(address _newAdmin) 
-        external 
-        onlyAdmin
-        onlyValidAddress(_newAdmin)
-    {
-        owner_ = _newAdmin;
-        emit ChangedAdmin(_newAdmin);
     }
 
 }
